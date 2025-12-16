40b61643aaf7c41bc95c7389d45943427949b608
Kurban Magomedov <cuba.pro@ya.ru>
2025-12-16 18:22:04 +0300

    Change routs

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CardsView from "@/views/CardsView.vue";
-import LearnView from "@/views/LearnView.vue";
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,24 +9,6 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView,
-        },
-        {
-            path: '/learn',
-            name: 'learn',
-            component: LearnView,
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue'),
-        },
-        {
-            path: '/project/:id/questions',
-            name: 'questions',
-            component: () => import('../views/QuestionsView.vue'),
         },
     ],
 })
